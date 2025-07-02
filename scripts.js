3482c98b8d712e4f47529f48621faba2ddabb86e
Rohan Paul <136823177+rohaney09@users.noreply.github.com>
2025-07-02 22:16:15 +0530

    scripts.js

--- scripts.js
+++ scripts.js
@@ -117,13 +117,3 @@ function Task() {
   }
   return false; // Allow form submission
 }
-
-/**
- echo "# Digital-Portfolio" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/rohaney09/Digital-Portfolio.git
-git push -u origin main
- */
